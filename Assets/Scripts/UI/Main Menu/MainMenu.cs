65c316965d48147a0c2ffc4e21bfb0d417ecfbd9
MarcosGermanCaravaggioSimon <mg.caravaggio.s@gmail.com>
2021-07-06 21:25:15 -0300

    Intento de subida
    Intento 2

---
+++ Assets/Scripts/UI/Main Menu/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public GameObject opciones;
+    public GameObject mainMenu;
+    public string nameScense;
+    public void Play()
+    {
+        SceneManager.LoadScene(nameScense);
+    }
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        opciones.SetActive(true);
+    }
+    public void Menu()
+    {
+        mainMenu.SetActive(true);
+        opciones.SetActive(false);
+    }
+    public void Exit()
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+}
